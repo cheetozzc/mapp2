92a61396f31b7775d7749c09dc18d238799413b3
cheetozzc <zzhan129@binghamton.edu>
2022-07-11 06:31:28 +0800

    11

---
+++ fub.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="fub" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="../wka/workadventure-develop/maps/assets/Special_Zones.png" width="192" height="64"/>
+</tileset>
